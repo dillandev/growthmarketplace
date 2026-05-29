f6901f7ad3be3dc84a825ae9d8bb3df5bc50e33e
dillandev <dillanhorne3@gmail.com>
2026-05-29 16:04:03 -0400

    Delete components directory

--- components/widgets/AppMessage.jsx
+++
@@ -1,7 +0,0 @@
-import { Alert } from "@mui/material";
-
-export default function AppMessage({ severity = "info", children }) {
-  if (!children) return null;
-
-  return <Alert severity={severity}>{children}</Alert>;
-}
